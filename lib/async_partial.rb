90cf1a98a297cb7a3c49ce63a95ef86b9358e29b
Akira Matsuda <ronnie@dio.jp>
2018-04-05 16:07:59 +0900

    Array based OutputBuffer that doesn't kick to_s on append

--- lib/async_partial.rb
+++ lib/async_partial.rb
@@ -28,4 +28,41 @@ module AsyncPartial
       val
     end
   end
+
+  module ArrayBuffer
+    def initialize(*)
+      super
+      @values = []
+      @caller = caller[0]
+    end
+
+    def <<(value)
+      @values << [value, :<<] unless value.nil?
+      self
+    end
+    alias :append= :<<
+
+    def safe_concat(value)
+      raise ActiveSupport::SafeBuffer::SafeConcatError unless html_safe?
+      @values << [value, :safe_concat] unless value.nil?
+      self
+    end
+    alias :safe_append= :safe_concat
+
+    def safe_expr_append=(val)
+      @values << [val, :safe_expr_append] unless val.nil?
+      self
+    end
+
+    def to_s
+      result = @values.each_with_object(ActiveSupport::SafeBuffer.new) do |(v, meth), buf|
+        if meth == :<<
+          buf << v.to_s
+        else
+          buf.safe_concat(v.to_s)
+        end
+      end
+      result.to_s
+    end
+  end
 end
