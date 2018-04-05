646143aba6e473d3b370182d36a4350da8a91c52
Akira Matsuda <ronnie@dio.jp>
2018-04-05 15:56:58 +0900

    Defer Thread#join until to_s call for the result

--- lib/async_partial.rb
+++ lib/async_partial.rb
@@ -6,10 +6,26 @@ module AsyncPartial
   module Renderer
     def render(context, options, block)
       if (options.delete(:async) || (options[:locals]&.delete(:async)))
-        Thread.new { super }
+        AsyncResult.new(Thread.new { super })
       else
         super
       end
+    end
+  end
+
+  class AsyncResult
+    def initialize(thread)
+      @thread = thread
+    end
+
+    def nil?
+      false
+    end
+
+    def to_s
+      val = @thread.value
+      @thread.kill
+      val
     end
   end
 end
