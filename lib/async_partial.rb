a060b3fd677e06955ebb51e981d43681c7bb7de2
Akira Matsuda <ronnie@dio.jp>
2018-04-05 15:54:07 +0900

    A simply threaded renderer

--- lib/async_partial.rb
+++ lib/async_partial.rb
@@ -1,5 +1,15 @@
+# frozen_string_literal: true
+
 require "async_partial/version"
 
 module AsyncPartial
-  # Your code goes here...
+  module Renderer
+    def render(context, options, block)
+      if (options.delete(:async) || (options[:locals]&.delete(:async)))
+        Thread.new { super }
+      else
+        super
+      end
+    end
+  end
 end
